3430b0ae8c27820cbd591637f05bba75f3e6f165
NS-5 <18056613516@163.com>
2025-09-19 11:29:15 +0800

    feat: 传送组件 - 案例之Modal组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { createPortal } from 'react-dom'
+// import { createPortal } from 'react-dom'
+
+import { Modal } from './components/Modal'
 
 // createPortal
 // 注意这是一个API，不是组件，他的作用是：将一个组件渲染到DOM的任意位置，跟Vue的Teleport组件类似。
@@ -13,7 +15,8 @@ import { createPortal } from 'react-dom'
 
 const App: React.FC= () => {
   return (
-    createPortal(<div>Portal Content</div>, document.body, 'portal')
+    <Modal />
+    // createPortal(<div>Portal Content</div>, document.body, 'portal')
   )
 }
 

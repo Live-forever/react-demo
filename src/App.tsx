02dde8ed1c3885dccfb2d9a2eb9da61b7e5c878e
NS-5 <18056613516@163.com>
2025-09-19 10:54:12 +0800

    feat: 受控组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import Test from './components/Test'
-import Test1 from './components/Test1'
-import Test2 from './components/Test2'
+import React, { useState } from 'react'
 
+// React 受控组件
+// 受控组件一般是指表单元素，表单的数据由React的 State 管理，更新数据时，需要手动调用setState()方法，更新数据。因为React没有类似于Vue的v-model，所以需要自己实现绑定事件。
+
+// 那为什么需要使用受控组件呢？
+// 使用受控组件可以确保表单数据与组件状态同步、便于集中管理和验证数据，同时提供灵活的事件处理机制以实现数据格式化和UI联动效果。
 
 function App() {
-  const fn = (params: string) => {
-    console.log('🚀 ~ :5 ~ 父组件的方法 params:', params)
+  const [value, setValue] = useState('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
   }
+
   return (
     <>
-      <Test
-        id={1}
-        obj={{ a: 1, b: 2 }}
-        arr={[1, 2, 3]}
-        cb={fn}
-        empty={null}
-        element={<div>测试</div>}
-        isGirl={false}
-        >
-          <div>123</div>
-        </Test>
-
-        <Test1 />
-        <Test2 />
+      <input type="text" value={value} onChange={handleChange}/>
+      <div>{value}</div>
     </>
   )
 }

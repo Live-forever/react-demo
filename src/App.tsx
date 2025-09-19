7e89e28daddc4a0ea9ebe939774a6a86ebda7a9e
NS-5 <18056613516@163.com>
2025-09-19 11:18:12 +0800

    feat: 非受控组件 - File 正确做法 ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 
 // React 非受控组件
 // 特殊的表单File
 // 对于file类型的表单控件，它是一个特殊的组件，因为它的值只能由用户通过文件选择操作来设置，
 // 而不能通过程序直接设置。这使得它在React中的处理方式与其他表单元素有所不同。
 
-// 如果非要把file类型设置为受控组件，他就会就行报错
+// 将其修改为非受控组件
 
 const App: React.FC= () => {
-  const [file, setFile] = useState<File | null>(null)
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(e.target.files?.[0]!)
+  const fileInputRef = useRef<HTMLInputElement>(null)
+  const handleChange = () => {
+    if (fileInputRef.current) {
+      const files = fileInputRef.current.files
+      console.log('🚀 ~ :15 ~ files:', files)
+    }
   }
-
-
+  
   return (
     <>
-      <input type="file" value={file} onChange={handleChange} />
+      <input type="file" onChange={handleChange} ref={fileInputRef} />
     </>
   )
 }
